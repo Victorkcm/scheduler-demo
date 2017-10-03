fe18e78ea2ce2e14e3801c6acc508ea34fcc9aa4
Victor Augusto Souza Adao - 160258 <ra160258@le20-10.grad.fee.unicamp.br>
2017-10-02 23:10:08 -0300

    programa final

--- src/main.c
+++ src/main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  int a[5];
-  int n;
-
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
-
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+int main ()
+{
+  int i,n,t,aux,j,in[6];
+  
+  
+  for (i=0;i<6;i++)
+  {
+    scanf ("%d", &in[i]);
   }
+  
+  n=in[5];
+  
 
+  for (t=1; t<=n ;t++)
+  {
+    aux=0;
+   for (j=0; j<5; j++)
+   {
+     if (t%in[j]==0)
+      { 
+	aux=1;
+	printf ("%d",j); 
+      }
+      
+   }
+   
+   if (aux==0)
+       printf ("-");
+   printf ("\n");
+  }
+  
+ 
+    
+  system ("pause");
   return 0;
 }
